7a3c8283a108619f208b5af99f9555954b79a558
frank.sativa <porameht@outlook.com>
2022-01-29 11:47:41 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<MyApp> {
+  String result = "0";
+  String expression = "";
+
+
+  buttonPressed(String value){
+    print(value);
+    setState(() {
+      if ( value == "CLEAR"){
+        result = "0";
+      } else if ( value == "." ) {
+        if (result.contains("."))  {
+          return;
+        } else {
+          result = result + value;
+        }
+      } else if ( value == "="){
+        expression = result.replaceAll("X", "*");
+        Parser p = Parser();
+        Expression exp = p.parse(expression);
+        ContextModel cm = ContextModel();
+        dynamic calculate = exp.evaluate(EvaluationType.REAL, cm);
+        result = "$calculate";
+      }else {
+        if ( result == "0" ){
+          result = value;
+        } else {
+          result = result + value;
+        }
+      }
+    });
+  }
+
+  Widget myButton(String buttonLabel){
+    return Expanded(
+      child: OutlineButton(
+        padding: const EdgeInsets.all(24),
+        onPressed:() => buttonPressed(buttonLabel),
+        child: Text(
+          buttonLabel,
+          style: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Calculator"),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  result,
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              const Expanded(
+                  child: Divider()
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      myButton("7"),
+                      myButton("8"),
+                      myButton("9"),
+                      myButton("/"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("4"),
+                      myButton("5"),
+                      myButton("6"),
+                      myButton("X"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("1"),
+                      myButton("2"),
+                      myButton("3"),
+                      myButton("-"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("."),
+                      myButton("0"),
+                      myButton("00"),
+                      myButton("+"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("CLEAR"),
+                      myButton("="),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
